25ca76e729118c8bce9ca55f629d4f8b323784e6
Johnson DUBULA <jdubula@c4r20s3.wethinkcode.co.za>
2020-01-18 11:34:09 +0200

    seting up the server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.listen(3001, function(){
+console.log("i am now live");
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
