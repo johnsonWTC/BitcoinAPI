efc1e041482f9ecbdf57e2ad571cdc15e804a4b6
Johnson DUBULA <jdubula@c4r20s3.wethinkcode.co.za>
2020-01-18 13:38:35 +0200

    req and res added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+// const request = require("request");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3001, function(){
+app.listen(3002, function(){
 console.log("i am now live");
 });
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+
+    var respond = req.body.crypto
+    console.log();
+    res.send("just sent " + respond);
+
+ 
+
 });
