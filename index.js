5c074a39a36e281a8778376c9de6cb3f6a3e1ed8
johnsonWTC <dubula@student.wethinkcode.co.za>
2020-01-20 08:18:29 +0200

    using write to display staff

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const request = require("request");
+ const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3002, function(){
+app.listen(3000, function(){
 console.log("i am now live");
 });
 
@@ -14,11 +14,18 @@ app.get("/",function(req,res){
 });
 
 app.post("/", function(req,res){
+    var cryptoC = req.body.crypto;
+    var currencyC = req.body.currency;
+    var per = cryptoC+currencyC;
+    var urlToSend = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"+ per;
 
-    var respond = req.body.crypto
-    console.log();
-    res.send("just sent " + respond);
-
+request(urlToSend, function(error,respond,body){
+   data =JSON.parse(body);
+   res.write("writting now");
+   res.write("Cryto type: " +cryptoC + " at this carrency "+ currencyC + " the value is "+ data.last)
+   console.log(data.last);
+   res.send();
+});
  
 
 });
